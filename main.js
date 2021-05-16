704fb95d40b1c3e972afd093680b0d9cbc67b597
Marina8989 <marina.mill209@gmail.com>
2021-05-15 22:36:32 -0700

    finished

--- main.js
+++ main.js
@@ -9,7 +9,17 @@ next.addEventListener('click', () => {
     activeCircle++;
     if(activeCircle > circles.length) {
        activeCircle = circles.length;
-
+    }
+    if(activeCircle == 2){
+        progressBar.style.width = '9%';
+        progressBar.classList.add('active');
+        circles[1].classList.add('active');
+    }else if(activeCircle == 3){
+        progressBar.style.width = '18%';
+        circles[2].classList.add('active');
+    }else if(activeCircle == 4) {
+        progressBar.style.width = '27%';
+        circles[3].classList.add('active');
     }
     console.log(activeCircle);
     progressStep();
@@ -20,13 +30,26 @@ prev.addEventListener('click', () => {
     if(activeCircle < 1) {
        activeCircle = 1;
     }
+    if(activeCircle == 4){
+        progressBar.style.width = '27%';
+        progressBar.classList.add('active');
+        circles[4].classList.remove('active');
+    }else if(activeCircle == 3){
+        progressBar.style.width = '18%';
+        circles[3].classList.remove('active');
+    }else if(activeCircle == 2) {
+        progressBar.style.width = '9%';
+        circles[2].classList.remove('active');
+    }else if(activeCircle == 1){
+        progressBar.style.width = '0%';
+        circles[1].classList.remove('active');
+    }
     console.log(activeCircle);
     progressStep();
 })
 
 function progressStep() {
-     //change the circle border
-     //change the progress bar
+     
      //change the buttons disabled state
      if(activeCircle === circles.length) {
         next.classList.add('disabled');
