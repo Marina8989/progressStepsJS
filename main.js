4e2a1c5971299d018cf7d0a41e097d7ad140fb56
Marina8989 <marina.mill209@gmail.com>
2021-05-15 22:06:17 -0700

    adding js

--- main.js
+++ main.js
@@ -9,8 +9,10 @@ next.addEventListener('click', () => {
     activeCircle++;
     if(activeCircle > circles.length) {
        activeCircle = circles.length;
+
     }
     console.log(activeCircle);
+    progressStep();
 })
 
 prev.addEventListener('click', () => {
@@ -19,4 +21,21 @@ prev.addEventListener('click', () => {
        activeCircle = 1;
     }
     console.log(activeCircle);
+    progressStep();
 })
+
+function progressStep() {
+     //change the circle border
+     //change the progress bar
+     //change the buttons disabled state
+     if(activeCircle === circles.length) {
+        next.classList.add('disabled');
+        prev.classList.remove('disabled');
+     }else if(activeCircle == 1) {
+       prev.classList.add('disabled');
+       next.classList.remove('disabled');
+     }else {
+         prev.classList.remove('disabled');
+         next.classList.remove('disabled');
+     }
+}
